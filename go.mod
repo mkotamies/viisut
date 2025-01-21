01864f41da4252a17426ded667d541f9e9c75077
Mikko Kotamies <mikko.kotamies@xa.epicgames.com>
2025-01-21 20:23:04 +0200

    chart: create simple chart for showing views

--- go.mod
+++ go.mod
@@ -1,11 +1,15 @@
 module viisut
 
-go 1.20
+go 1.22.0
+
+toolchain go1.23.3
 
 require (
+	github.com/go-echarts/go-echarts/v2 v2.4.6 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.3 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -10,6 +10,9 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"github.com/go-echarts/go-echarts/v2/types"
 	"github.com/jackc/pgx/v5"
 )
 
@@ -52,6 +55,120 @@ LEFT JOIN LATERAL (
 	return contestants
 }
 
+func getTimeInterval(conn *pgx.Conn) []time.Time {
+	var updateTimes []time.Time
+	query := `SELECT DISTINCT updated AS count FROM statistic`
+
+	rows, err := conn.Query(context.Background(), query)
+	if err != nil {
+		log.Fatalf("Query failed: %v\n", err)
+	}
+
+	for rows.Next() {
+		var updated time.Time
+		err := rows.Scan(&updated)
+		if err != nil {
+			fmt.Println(err)
+		}
+		updateTimes = append(updateTimes, updated)
+
+	}
+
+	return updateTimes
+}
+
+type Views []struct {
+	Count   int
+	Updated time.Time
+}
+type ContestantViews struct {
+	VideoId    string
+	Name       string
+	ViewCounts Views
+}
+
+func AddOrUpdateContestantView(contestants []ContestantViews, videoId, name string, viewCount int, updated time.Time) []ContestantViews {
+	if contestants == nil {
+		contestants = append(contestants, ContestantViews{VideoId: videoId, Name: name, ViewCounts: Views{{Count: viewCount, Updated: updated}}})
+		return contestants
+	}
+	for i := range contestants {
+		if (contestants)[i].VideoId == videoId {
+			(contestants)[i].ViewCounts = append((contestants)[i].ViewCounts, struct {
+				Count   int
+				Updated time.Time
+			}{
+				Count:   viewCount,
+				Updated: updated,
+			})
+		}
+		contestants = append(contestants, ContestantViews{VideoId: videoId, Name: name, ViewCounts: Views{{Count: viewCount, Updated: updated}}})
+	}
+	return contestants
+}
+
+func getContestantViews(conn *pgx.Conn) []ContestantViews {
+	var contestantViews []ContestantViews
+	rows, err := conn.Query(context.Background(), `SELECT
+    c.video_id, c.name, s.view_count, s.updated FROM
+    contestant as c
+LEFT JOIN LATERAL (
+    SELECT s.view_count, s.updated
+    FROM statistic as s
+    WHERE s.video_id = c.video_id
+    ORDER BY s.updated ASC
+) s ON true`)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	for rows.Next() {
+		var name string
+		var view_count int
+		var updated time.Time
+		var video_id string
+
+		err := rows.Scan(&video_id, &name, &view_count, &updated)
+		if err != nil {
+			fmt.Println(err)
+		}
+		contestantViews = AddOrUpdateContestantView(contestantViews, video_id, name, view_count, updated)
+	}
+	return contestantViews
+}
+
+func generateLineItems(viewCounts Views) []opts.LineData {
+	items := make([]opts.LineData, 0)
+	for j := range viewCounts {
+		items = append(items, opts.LineData{Value: viewCounts[j].Count})
+	}
+	return items
+}
+
+func httpserver(conn *pgx.Conn) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var timeInterval = getTimeInterval(conn)
+		var contestantViews = getContestantViews(conn)
+
+		line := charts.NewLine()
+		line.SetGlobalOptions(
+			charts.WithInitializationOpts(opts.Initialization{Theme: types.ThemeWesteros}),
+			charts.WithTitleOpts(opts.Title{
+				Title:    "Video trends over time",
+				Subtitle: "Video views",
+			}))
+
+		var xAxis = line.SetXAxis(timeInterval)
+
+		for i := range contestantViews {
+			var contestant = contestantViews[i]
+			xAxis.AddSeries(contestant.Name, generateLineItems(contestant.ViewCounts))
+		}
+		xAxis.SetSeriesOptions(charts.WithLineChartOpts(opts.LineChart{Smooth: opts.Bool(true)}))
+		line.Render(w)
+	}
+}
+
 func main() {
 	fmt.Println("Go app...")
 
@@ -72,6 +189,7 @@ func main() {
 
 	// define handlers
 	http.HandleFunc("/", h1)
+	http.HandleFunc("/chart", httpserver(conn))
 
 	log.Fatal(http.ListenAndServe("localhost:9000", nil))
 
